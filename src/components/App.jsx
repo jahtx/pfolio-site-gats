a3097430a3444d91becb332ab9d6e3949c375557
James Hernandez <tech@jahtx.com>
2021-03-27 23:20:47 -0500

    playing around with SourceTree commits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from "react"
 import Header from "./Header/Header"
+import Body from "./Body/Body"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <Body></Body>
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,31 @@
 import React from "react"
-import "bootstrap/dist/css/bootstrap.min.css"
 
-import { Navbar } from "react-bootstrap"
+import { Navbar, Nav, NavDropdown } from "react-bootstrap"
 
 // markup
 const Header = () => {
   return (
     <div>
-      <Navbar bg="dark">
-        <Navbar.Brand href="#home">
-          <div class="container-lg">
-            <h1 style={{ color: `white`, fontSize: `36px` }}>
-              James Hernandez
-            </h1>
-          </div>
-        </Navbar.Brand>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   )
