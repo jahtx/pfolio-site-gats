de8699ec553e7f86d8685412ff28eedc5c3e88b1
James Hernandez <tech@jahtx.com>
2021-03-27 23:22:29 -0500

    testing again

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import { Nav } from "react-bootstrap"
+
+const Body = () => {
+  return (
+    <div>
+      <Nav></Nav>
+      Huh
+    </div>
+  )
+}
+
+export default Body
---
+++ src/pages/portfolio.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import App from "../components/App"
+
+// markup
+const PortfolioPage = () => {
+  return <App></App>
+}
+
+export default PortfolioPage
