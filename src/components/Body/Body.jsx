860cc821c6abb7adc7d0794afc9a49cd3d1c0cb7
James Hernandez <tech@jahtx.com>
2021-04-05 14:52:28 -0500

    added texas logo

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,11 +1,7 @@
 import React from "react"
 
 const Body = () => {
-  return (
-    <div>
-      <div className="container-fluid jah-topBar">here</div>
-    </div>
-  )
+  return <div></div>
 }
 
 export default Body
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from "react"
+import "./header.scss"
 
 // markup
 const Header = () => {
-  return <div className="container-fluid">Testing</div>
+  return (
+    <div>
+      <div className="container-fluid jah-topBar"></div>
+      <div className="container-fluid jah-txSpacer"></div>
+    </div>
+  )
 }
 
 export default Header
