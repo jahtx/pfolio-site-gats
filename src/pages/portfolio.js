7c354f6d2fddc914a45b950f0156bbe0c2cda3f5
James Hernandez <tech@jahtx.com>
2021-04-10 19:39:59 -0500

    for the love of benji, i got GatsbyImage working

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
 //import Modal from "react-modal"
 import Header from "../components/theme/Header/Header"
 import Footer from "../components/theme/Footer/Footer"
@@ -8,26 +8,33 @@ import Composite from "../components/common/Composite/Composite"
 import "../styles/main.scss"
 
 const PortfolioPage = ({ data }) => {
-  console.log("from portfolio page: " + JSON.stringify(data.file.id))
+  // console.log(
+  //   "from portfolio page: " + JSON.stringify(data.file.childImageSharp.fluid)
+  // )
+
+  // const bob = getImage(data.image.)
   return (
     <div id="root">
       <Header></Header>
       <div className="container-lg">
+        <GatsbyImage
+          image={getImage(data.image.childImageSharp.gatsbyImageData)}
+          alt="spongebob"
+        />
+        {/* <p>{data.file.childImageSharp.fluid}</p> */}
         {/* <p>{data.site.siteMetadata.title}</p> */}
       </div>
-      <Composite title={data.file.id} />
+      {/* <Composite title={data.file.id} /> */}
       <Footer></Footer>
     </div>
   )
 }
 export const query = graphql`
   query PortfolioQuery {
-    file(relativePath: { eq: "comp.png" }) {
+    image: file(relativePath: { eq: "bob.png" }) {
       id
       childImageSharp {
-        fluid(maxWidth: 10) {
-          ...GatsbyImageSharpFluid
-        }
+        gatsbyImageData(quality: 90, width: 400, layout: CONSTRAINED)
       }
     }
   }
