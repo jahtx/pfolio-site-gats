4b66e98e0951f7eb266292aa6b95bdac47c71459
James Hernandez <tech@jahtx.com>
2021-04-10 21:31:50 -0500

    finally getting a rythem

--- src/components/common/Composite/Composite.js
+++ src/components/common/Composite/Composite.js
@@ -1,43 +1,27 @@
 import React, { useState } from "react"
-import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 import { Button, Modal } from "react-bootstrap"
-// import { graphql } from "gatsby"
 import "./composite.scss"
 const Composite = props => {
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
 
-  const imageLocation = "../../../images/comp.png"
-  // console.log({ data })
-  // console.log("from composite page: " + JSON.stringify({ data }))
   return (
     <div>
-      {/* <p>{data.site.siteMetadata.description}</p> */}
-
       <div className="j-thumbnail">
-        <StaticImage
-          src={imageLocation}
-          alt="black hex"
-          loading="lazy"
+        <GatsbyImage
+          image={props.imageInfo}
+          alt={props.altInfo}
           onClick={handleShow}
         />
       </div>
-      {/* <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button> */}
-
       <Modal dialogClassName="j-modal" show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>{props.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body className="j-modalBody">
-          <StaticImage
-            src={imageLocation}
-            alt="black hex"
-            loading="lazy"
-            width={200}
-          />
+          <GatsbyImage image={props.imageInfo} alt={props.altInfo} />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 //import Modal from "react-modal"
 import Header from "../components/theme/Header/Header"
 import Footer from "../components/theme/Footer/Footer"
@@ -8,33 +8,38 @@ import Composite from "../components/common/Composite/Composite"
 import "../styles/main.scss"
 
 const PortfolioPage = ({ data }) => {
-  // console.log(
-  //   "from portfolio page: " + JSON.stringify(data.file.childImageSharp.fluid)
-  // )
+  console.log(data)
 
-  // const bob = getImage(data.image.)
   return (
     <div id="root">
       <Header></Header>
       <div className="container-lg">
-        <GatsbyImage
-          image={getImage(data.image.childImageSharp.gatsbyImageData)}
-          alt="spongebob"
+        <Composite
+          title="Checking Mobile"
+          imageInfo={getImage(data.checkingMobile)}
+          altInfo="Checking Mobile"
         />
-        {/* <p>{data.file.childImageSharp.fluid}</p> */}
-        {/* <p>{data.site.siteMetadata.title}</p> */}
       </div>
-      {/* <Composite title={data.file.id} /> */}
       <Footer></Footer>
     </div>
   )
 }
 export const query = graphql`
   query PortfolioQuery {
-    image: file(relativePath: { eq: "bob.png" }) {
+    autoLoansMobile: file(
+      relativePath: { eq: "composites/RBFCU_Auto_Loans_1.png" }
+    ) {
       id
       childImageSharp {
-        gatsbyImageData(quality: 90, width: 400, layout: CONSTRAINED)
+        gatsbyImageData(quality: 90, width: 300, layout: CONSTRAINED)
+      }
+    }
+    checkingMobile: file(
+      relativePath: { eq: "composites/RBFCU_Checking_1.png" }
+    ) {
+      id
+      childImageSharp {
+        gatsbyImageData(quality: 90, width: 300, layout: CONSTRAINED)
       }
     }
   }
