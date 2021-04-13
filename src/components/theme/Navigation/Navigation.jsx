1d02eaf80fbdc8bf64492afc416185483be58989
James Hernandez <tech@jahtx.com>
2021-04-13 16:25:12 -0500

    made my name a link to the home page and cleanup

--- src/components/theme/Navigation/Navigation.jsx
+++ src/components/theme/Navigation/Navigation.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">James A. Hernandez</Navbar.Brand>
+        <Navbar.Brand href="/">James A. Hernandez</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
