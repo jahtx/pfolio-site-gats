928ac7efe830434650c6e5f86e01703836d69978
James Hernandez <tech@jahtx.com>
2021-04-14 00:56:18 -0500

    added title and nice favicon

--- src/components/theme/Header/Header.jsx
+++ src/components/theme/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react"
 import "./header.scss"
+import { Helmet } from "react-helmet"
 
 // markup
 const Header = () => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>James A. Hernandez | UX Designer and Front-end Developer</title>
+      </Helmet>
+
       <div className="container-fluid j-topBar"></div>
     </div>
   )
