fc220651178d5b6e3bc7a2998c0cc8a469b4fd77
James Hernandez <tech@jahtx.com>
2021-04-28 13:25:22 -0500

    footer images now graphql

--- src/components/theme/Footer/Footer.jsx
+++ src/components/theme/Footer/Footer.jsx
@@ -8,9 +8,16 @@ const Footer = () => {
   const data = useStaticQuery(
     graphql`
       query {
-        beltStitch: file(relativePath: { eq: "belt-stitch.png" }) {
+        beltStitch: file(relativePath: { eq: "footer/belt-stitch.png" }) {
           childImageSharp {
             fluid(quality: 100, maxWidth: 35) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        star: file(relativePath: { eq: "footer/star.png" }) {
+          childImageSharp {
+            fluid(quality: 100, maxWidth: 100) {
               ...GatsbyImageSharpFluid_withWebp
             }
           }
@@ -19,13 +26,18 @@ const Footer = () => {
     `
   )
   const beltStitch = data.beltStitch.childImageSharp.fluid
+  const star = data.star.childImageSharp.fluid
   return (
     <BackgroundImage
       tag="div"
       className="container-fluid bottomBar"
       fluid={beltStitch}
     >
-      <div className="buckle"></div>
+      <BackgroundImage
+        tag="div"
+        className="buckle"
+        fluid={star}
+      ></BackgroundImage>
     </BackgroundImage>
   )
 }
