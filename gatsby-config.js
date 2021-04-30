2e177d9ee755505776713c297f9985b194cfbc49
James Hernandez <tech@jahtx.com>
2021-04-29 21:19:56 -0500

    got rid of conversion Id

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,11 +26,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          'G-VFZEZQXCGB', // Google Analytics / GA
-          'AW-CONVERSION_ID', // Google Ads / Adwords / AW
-          'DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
+        trackingIds: ['G-VFZEZQXCGB'],
       },
     },
     {
