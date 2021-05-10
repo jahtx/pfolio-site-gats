1c0bb8797c46aa04eb31b24607d9ee4793c8dcbe
James Hernandez <tech@jahtx.com>
2021-05-10 15:36:34 -0500

    small changes

--- src/components/theme/Footer/Footer.jsx
+++ src/components/theme/Footer/Footer.jsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+
 import PropTypes from 'prop-types'
-import BackgroundImage from 'gatsby-background-image'
 import './footer.scss'
 
 // markup
 const Footer = props => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        beltStitch: file(relativePath: { eq: "footer/belt-stitch.png" }) {
-          childImageSharp {
-            fluid(quality: 100, maxWidth: 35) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-      }
-    `
-  )
-  const beltStitch = data.beltStitch.childImageSharp.fluid
-
   return (
     <React.Fragment>
       {props.showVaccineMessage ? (
@@ -36,11 +20,6 @@ const Footer = props => {
         </div>
       ) : null}
 
-      {/* <BackgroundImage
-        tag="div"
-        className="container-fluid bottomBar"
-        fluid={beltStitch}
-      ></BackgroundImage> */}
       <p className="footerTrailer text-center mb-5">
         Built with <a href="https://reactjs.org/">React</a> and{' '}
         <a href="https://www.gatsbyjs.com/">Gatsby</a>.
