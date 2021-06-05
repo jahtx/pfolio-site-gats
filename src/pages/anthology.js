3c0bf0f5859967c7a3a5bb3caa79fd0402cbc5e1
James Hernandez <tech@jahtx.com>
2021-06-04 19:44:41 -0500

    finally got the portfolio page done

--- src/pages/anthology.js
+++ src/pages/anthology.js
@@ -14,7 +14,7 @@ const AnthologyPage = () => {
         rbfcuPhone: file(relativePath: { eq: "phone-with-rbfcu.png" }) {
           childImageSharp {
             gatsbyImageData(
-              width: 270
+              width: 240
               quality: 85
               placeholder: BLURRED
               formats: [WEBP]
@@ -32,32 +32,23 @@ const AnthologyPage = () => {
     <MainLayout showVacMsg={false}>
       <SectionAnthRbBackground>
         <Container as="section" fluid="lg" className="p-4 d-flex exBox">
-          <div className="exBox__part1 m-0">
-            <h2 className="one-pt-1-rem all-caps mt-3 mb-3">
-              Randolph-Brooks Federal Credit Union
-            </h2>
-            <p className="pt-9-rem">
-              Randolph-Brooks Federal Credit Union is an accredited financial
-              institution headquartered in Live Oak, Texas and is the second
-              largest credit union in Texas.
-            </p>
-            <p className="pt-9-rem">
-              Their website had long been overdue for a refreshed design and
-              needed to meet the expectations of a younger and more
-              technologically demanding customer base.
-            </p>
-            <p className="pt-9-rem">
-              Challenges included accounting for over a thousand disparate pages
-              and content, accessibility, and incorporating a new content
-              management system. Over a period of nine months, James was
-              instrumental in prioritizing product and services content,
-              developing user research, and leading the front-end development
-              effort.
-            </p>
-            <div className="sect-btn2 w-100">
-              <Button size="sm" variant="primary" href="/portfolio">
-                See More
-              </Button>
+          <div className="exBox__part1 m-0 d-flex justify-content-center align-items-center">
+            <div>
+              {' '}
+              <h2 className="one-pt-1-rem all-caps mt-3 mb-3">
+                Randolph-Brooks Federal Credit Union
+              </h2>
+              <p className="pt-9-rem">
+                This effort encompassed a full redesign of a company’s primary
+                website, including a thorough review of existing pages and their
+                metrics, discussions of executive inititives and risks, and a
+                development of a comprehensive templating system.
+              </p>
+              <div className="sect-btn2 w-100">
+                <Button size="sm" variant="primary" href="/portfolio">
+                  See More
+                </Button>
+              </div>
             </div>
           </div>
           <div className="exBox__part2 d-flex justify-content-center align-items-center">
